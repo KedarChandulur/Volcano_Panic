d864de478c58ae0b3767644e0309b8517c952209
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2024-04-26 03:39:10 -0500

    Updated ambulance and fence settings

--- Assets/Scripts/RescueVechicles.cs
+++ Assets/Scripts/RescueVechicles.cs
@@ -5,13 +5,12 @@ using UnityEngine.AI;
 
 public class RescueVechicles : MonoBehaviour
 {
-    [SerializeField]
     NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-        //this.agent = GetComponent<NavMeshAgent>();
+        this.agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(new Vector3(234.26f, 0.0f, 142.58f));
     }
 
